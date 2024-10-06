5950b986574ac4743d2647ad4b81f264e5f4d92a
bignyap <bignyap@wolfram.com>
2024-10-06 19:55:13 +0530

    Added subscription tier endpoint

---
+++ database/sqlc/queries/sub_tier.sql
@@ -0,0 +1,11 @@
+-- name: ListSubscriptionTier :many
+SELECT * FROM subscription_tier
+ORDER BY tier_name;
+
+-- name: CreateSubscriptionTier :execresult 
+INSERT INTO subscription_tier (tier_name, tier_description, tier_created_at, tier_updated_at) 
+VALUES (?, ?, ?, ?);
+
+-- name: DeleteSubscriptionTierById :exec
+DELETE FROM subscription_tier
+WHERE subscription_tier_id = ?;
---
+++ handler/subscriptiontier.go
@@ -0,0 +1,134 @@
+package handler
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/bignyap/go-gate-keeper/database/sqlcgen"
+)
+
+type CreateSubTierParams struct {
+	Name        string    `json:"name"`
+	Description *string   `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type CreateSubTierOuput struct {
+	ID int `json:"id"`
+	CreateSubTierParams
+}
+
+func (apiCfg *ApiConfig) CreateSubTier(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		respondWithError(w, 400,
+			fmt.Sprintf("Error parsing form data: %s", err),
+		)
+		return
+	}
+
+	input := sqlcgen.CreateSubscriptionTierParams{
+		TierName: r.FormValue("name"),
+		TierDescription: sql.NullString{
+			String: r.FormValue("description"),
+			Valid:  r.FormValue("description") != "",
+		},
+		TierCreatedAt: time.Now(),
+		TierUpdatedAt: time.Now(),
+	}
+
+	if input.TierName == "" {
+		respondWithError(w, 400, "Name is required")
+		return
+	}
+
+	subTier, err := apiCfg.DB.CreateSubscriptionTier(r.Context(), input)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("couldn't create the subscription tier: %s", err))
+		return
+	}
+
+	insertedID, err := subTier.LastInsertId()
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("couldn't retrieve last insert ID: %s", err))
+		return
+	}
+
+	var description *string
+	if input.TierDescription.Valid {
+		description = &input.TierDescription.String
+	}
+
+	output := CreateSubTierOuput{
+		ID: int(insertedID),
+		CreateSubTierParams: CreateSubTierParams{
+			Name:        input.TierName,
+			Description: description,
+			CreatedAt:   input.TierCreatedAt,
+			UpdatedAt:   input.TierUpdatedAt,
+		},
+	}
+
+	respondWithJSON(w, 201, output)
+}
+
+func (apiCfg *ApiConfig) ListSubTiers(w http.ResponseWriter, r *http.Request) {
+
+	subTiers, err := apiCfg.DB.ListSubscriptionTier(r.Context())
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("couldn't retrieve the subscripion tiers: %s", err))
+		return
+	}
+
+	var output []CreateSubTierOuput
+
+	for _, subTier := range subTiers {
+
+		var description *string
+		if subTier.TierDescription.Valid {
+			description = &subTier.TierDescription.String
+		}
+
+		output = append(output, CreateSubTierOuput{
+			ID: int(subTier.SubscriptionTierID),
+			CreateSubTierParams: CreateSubTierParams{
+				Name:        subTier.TierName,
+				Description: description,
+				CreatedAt:   subTier.TierCreatedAt,
+				UpdatedAt:   subTier.TierUpdatedAt,
+			},
+		})
+	}
+
+	respondWithJSON(w, 201, output)
+}
+
+func (apiCfg *ApiConfig) DeleteSubTier(w http.ResponseWriter, r *http.Request) {
+
+	idStr := r.URL.Query().Get("id")
+	if idStr == "" {
+		respondWithError(w, 400, "ID is required")
+		return
+	}
+
+	id64, err := strconv.ParseInt(idStr, 10, 32)
+	if err != nil {
+		respondWithError(w, 400, "Invalid ID format")
+		return
+	}
+
+	err = apiCfg.DB.DeleteSubscriptionTierById(r.Context(), int32(id64))
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("couldn't delete the subscription tier: %s", err))
+		return
+	}
+
+	respondWithJSON(w, 200, map[string]string{
+		"message": fmt.Sprintf("Subscription tier with ID %d deleted successfully", int32(id64)),
+	})
+}
